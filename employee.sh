d7e57fcef0b14e030699aec66e9fbe7bf52366d5
jay <jay@localhost.localdomain>
2021-01-30 15:42:41 +0400

    added basic bash files

---
+++ employee.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "Please enter your username"
+read username
+
+if [ "$username" = "mark" ];
+then
+	echo "Welcome back  mark"
+else
+	echo "Soryy user not fournd!!"
+fi
---
+++ userinfo.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "Enter First and Last Name"
+read firstAndLastName
+echo "Enter Age"
+read age
+echo "Enter  Country"
+read country
+echo "My name is $firstAndLastName  and i am $age years old from $country"
+
+
